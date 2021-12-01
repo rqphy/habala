34581044f5ed45ba43b87a2177dfe14a93c1b679
charlescsn <c-cousin@hotmail.fr>
2021-12-01 15:53:01 +0100

    renamed + created PostController

---
+++ src/controller/PostController.php
@@ -0,0 +1,8 @@
+<?php
+abstract class PostController {
+
+    protected $params;
+    protected $template = __DIR__ . './../Views/template.php';
+    protected $viewsDir = __DIR__ . './../Views/';
+
+}
---
+++ src/model/User.php
@@ -0,0 +1,91 @@
+<?php
+Class User {
+
+    public int $id;
+    public string $username;
+    public string $email;
+    public string $hashedPassword;
+    public bool $isAdmin;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHashedPassword(): string
+    {
+        return $this->hashedPassword;
+    }
+
+    /**
+     * @param string $hashedPassword
+     */
+    public function setHashedPassword(string $hashedPassword): void
+    {
+        $this->hashedPassword = $hashedPassword;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAdmin(): bool
+    {
+        return $this->isAdmin;
+    }
+
+    /**
+     * @param bool $isAdmin
+     */
+    public function setIsAdmin(bool $isAdmin): void
+    {
+        $this->isAdmin = $isAdmin;
+    }
+
+
+}

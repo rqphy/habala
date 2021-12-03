101d6a0f2dbb79894f12488da04142c806923bba
tLiedtsHetic <thomas.liedts@hetic.net>
2021-12-03 10:37:08 +0100

    add routing and views

--- src/index.php
+++ src/index.php
@@ -1,1 +1,16 @@
 <?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/':
+        require __DIR__ . '/view/home.php';
+        break;
+    case 'admin':
+        require __DIR__ . '/view/admin.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/view/404.php';
+        break;
+}

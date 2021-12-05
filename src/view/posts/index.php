8c4afd6488c0af372f475951b85aa83fc0244457
Raphael <raphael.ferreira@hetic.net>
2021-12-05 14:37:10 +0100

    change sm

--- src/view/posts/index.php
+++ src/view/posts/index.php
@@ -1,14 +1,16 @@
 <?php
 $posts = $data;
 
+echo __DIR__ . '/../../static/images/';
+
 if ($posts)
 {
 	foreach ($posts as $post)
 	{
 		?>
-		<article>
+		<article style="border: 1px solid tomato; margin-top: 20px;">
 			<p><?= $post['title'] ?></p>
-			<img src="<?= $post['image_url'] ?>" alt="<?= $post['title'] ?>">
+			<img src="../../public/images/<?= $post['image_url'] ?>" alt="<?= $post['title'] ?>">
 			<p><?= $post['content'] ?></p>
 			<p><?= $post['author_id'] ?></p>
 			<p><?= $post['date'] ?></p>
--- src/view/template.php
+++ src/view/template.php
@@ -5,6 +5,20 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title ?></title>
+
+        <style>
+          * {
+            margin: 0px;
+            padding: 0px;
+          }
+          img {
+            width: 100%;
+          }
+          article {
+            width: 90%;
+            max-width: 200px;
+          }
+        </style>
     </head>
 
     <body>
